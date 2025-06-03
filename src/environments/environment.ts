f27b3710a4808fd107af3c0c2e349f7f534849da
Khabir <hakim1khabir2@gmail.com>
2025-06-03 13:03:39 +0100

    change localhost to ngrok prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:9090/demo_war_exploded/api'
+  // apiUrl: 'http://localhost:9090/demo_war_exploded/api'
+  apiUrl: 'https://16ae-105-74-67-53.ngrok-free.app/demo_war_exploded/api'
 };
