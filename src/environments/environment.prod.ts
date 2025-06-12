204b84893afcc0808bcc5ec71b6e384bc0168f28
Abdessadek <abdessadekhm@gmail.com>
2025-06-12 07:31:29 +0100

    reactive prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://16ae-105-74-67-53.ngrok-free.app/demo_war_exploded/api'
-  apiUrl: 'http://localhost:9090/demo_war_exploded/api'
+  apiUrl: 'https://16ae-105-74-67-53.ngrok-free.app/demo_war_exploded/api'
 };
